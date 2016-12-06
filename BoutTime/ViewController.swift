ce873f9effda6d3fd265bf64cc74ac4799a44027
acahet <anderson.cahet@hotmail.com>
2016-12-06 12:57:05 +0000

    remove test

--- BoutTime/ViewController.swift
+++ BoutTime/ViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-        let test = 1 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
