1770747bc24f04f84015122f6c8cbcb253951aa0
acahet <anderson.cahet@hotmail.com>
2016-12-06 22:18:47 +0000

    Create a folder to hold controllers; create controller to holder the view where the results will be displayed; create a swift file to hold the historic events data. added labels, buttons to view controller

---
+++ BoutTime/HistoricEvents.swift
@@ -0,0 +1,11 @@
+//
+//  HistoricEvents.swift
+//  BoutTime
+//
+//  Created by Anderson Cahet on 06/12/2016.
+//  Copyright © 2016 Anderson Cahet. All rights reserved.
+//
+
+import Foundation
+
+//historic event//year// url:
---
+++ ResultsController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultsController.swift
+//  BoutTime
+//
+//  Created by Anderson Cahet on 06/12/2016.
+//  Copyright © 2016 Anderson Cahet. All rights reserved.
+//
+
+import UIKit
+
+class ResultsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
